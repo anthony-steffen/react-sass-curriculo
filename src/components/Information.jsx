4d2d8fb8fe8dce8115767bf4e98e55012b834250
Anthony Steffen <anthony.steffen@outlook.com.br>
2023-12-06 12:02:00 -0300

    Informações

--- src/components/Information.jsx
+++ src/components/Information.jsx
@@ -9,21 +9,21 @@ function Information() {
         <AiFillPhone id="phone-icon" />
         <div>
           <h3>Telefone</h3>
-          <p>(48)99999-9999</p>
+          <p>(84)99142-3988</p>
         </div>
       </div>
       <div className="info-card">
         <AiOutlineMail id="email-icon" />
         <div>
           <h3>E-mail</h3>
-          <p>matheus@gmail.com</p>
+          <p>anthony.steffen@outlook.com</p>
         </div>
       </div>
       <div className="info-card">
         <AiFillEnvironment id="pin-icon" />
         <div>
           <h3>Localização</h3>
-          <p>São José / SC</p>
+          <p>Baraúna/ RN</p>
         </div>
       </div>
     </section>
